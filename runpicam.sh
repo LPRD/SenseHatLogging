638c2ebb9ecade1007f8d8995266a6ccaf403a42
Lucas Kramer <krame505@umn.edu>
2016-04-21 21:46:07 -0500

    Adding pi scripts

---
+++ runpicam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+while true
+do
+    FILENAME=/home/pi/Desktop/SenseHatLogging/video_`date +%Y-%m-%d.%H:%M:%S`.h264
+    echo "Saving video to $FILENAME"
+    raspivid -o $FILENAME -t 600000
+done
+
---
+++ sensor.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+from sense_hat import SenseHat
+from datetime import datetime
+
+#Logging Settings
+FILENAME = "/home/pi/Desktop/SenseHatLogging/log"
+WRITE_FREQUENCY = 50 #adds 50 lines to file at once
+dateString = '%Y/%m/%d %H:%M:%S'
+
+
+#FUNCTIONS
+def file_setup(filename):
+  header  =["temp_h","temp_p","humidity","pressure",
+  "pitch","roll","yaw",
+  "mag_x","mag_y","mag_z",
+  "accel_x","accel_y","accel_z",
+  "gyro_x","gyro_y","gyro_z",
+  "timestamp"]
+
+  with open(filename,"w") as f:
+      f.write(",".join(str(value) for value in header)+ "\n")
+
+def log_data():
+  output_string = ",".join(str(value) for value in sense_data)
+  batch_data.append(output_string)
+
+def get_sense_data():
+  sense_data=[]
+
+  sense_data.append(sense.get_temperature_from_humidity())
+  sense_data.append(sense.get_temperature_from_pressure())
+  sense_data.append(sense.get_humidity())
+  sense_data.append(sense.get_pressure())
+
+  o = sense.get_orientation()
+  yaw = o["yaw"]
+  pitch = o["pitch"]
+  roll = o["roll"]
+  sense_data.extend([pitch,roll,yaw])
+
+  mag = sense.get_compass_raw()
+  mag_x = mag["x"]
+  mag_y = mag["y"]
+  mag_z = mag["z"]
+  sense_data.extend([mag_x,mag_y,mag_z])
+
+  acc = sense.get_accelerometer_raw()
+  x = acc["x"]
+  y = acc["y"]
+  z = acc["z"]
+  sense_data.extend([x,y,z])
+
+  gyro = sense.get_gyroscope_raw()
+  gyro_x = gyro["x"]
+  gyro_y = gyro["y"]
+  gyro_z = gyro["z"]
+  sense_data.extend([gyro_x,gyro_y,gyro_z])
+
+  sense_data.append(datetime.now())
+
+  return sense_data
+
+#MAIN PROGRAM
+
+sense = SenseHat()
+
+batch_data= []
+
+if FILENAME == "":
+  filename = "SenseLog-"+str(datetime.now())+".csv"
+else:
+  filename = FILENAME+"-"+str(datetime.now())+".csv"
+
+file_setup(filename)
+
+while True:
+  sense_data = get_sense_data()
+  log_data()
+
+  if len(batch_data) >= WRITE_FREQUENCY:
+      print("Writing to file..")
+      with open(filename,"a") as f:
+          for line in batch_data:
+              f.write(line + "\n")
+          batch_data = []
+
+
